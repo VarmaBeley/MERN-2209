1962dd837a2af1065b8ba69ba9f71ba04724e3f2
siddurajesh <siddu.rajesh1947@gmail.com>
2022-12-09 22:41:52 -0800

    mofified

---
+++ git-stash-reset/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This  Remote Repositary created by Varma </h1>
+      <h1>This is a Remote Repositary created by Rajeshwar</h1>
+      <br></br>
+      <h1>Remote Repositary pulled by Varma and modified and pushed by Varma</h1>
+      <h1>This heading added by Harish kumar</h1>
+      <h1>pulled by Rajeshwar and pushed</h1>
+    </div>
+  );
+}
+
+export default App;
