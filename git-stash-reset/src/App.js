dba624f689e912cff8f6dce97da3146afa1ef66d
siddurajesh <siddu.rajesh1947@gmail.com>
2022-12-10 05:11:26 -0800

    added some headings

--- git-stash-reset/src/App.js
+++ git-stash-reset/src/App.js
@@ -4,10 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This  Remote Repositary created by Varma </h1>
+      
       <h1>This is a Remote Repositary created by Rajeshwar</h1>
       <br></br>
-      <h1>Remote Repositary pulled by Varma and modified and pushed by Varma</h1>
       <h1>This heading added by Harish kumar</h1>
       <h1>pulled by Rajeshwar and pushed</h1>
       <h1>virat kohli</h1>
